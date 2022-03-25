0e3df2ad71cac6b10c65f5ac3372fd40c028ff27
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2022-03-25 15:46:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,134 @@
-maincon = document.getElementById("mainContent");
-// repolist = document.getElementById("repolist");
-next = document.getElementById("next");
-prev = document.getElementById("previous");
-maincon.remove();
-var list = new Array();
-var pageList = new Array();
-var currPage = 1;
-var numPerPage = 5;
-var numOfPage = 0;
-async function getapi(url){
-    const response = await fetch(url)
-    var data = await response.json()
-    bgcolor = ["#6f42c1", "#007bff", "#e83e8c", "#0dd909c7"]
-    for(i=0; i<data["length"]; i++){
-        nam = data[i]["name"];        
-        var l = await fetch(data[i]["languages_url"]);
-        var lang = await l.json()
-        typel = Object.keys(lang)[0]
-        console.log(typel)
-        if(typel==null || typel==undefined)
-            typel = ""
-        list.push("<div style='background-color:"+bgcolor[i%4]+"; border-radius: 0.25rem!important; padding: 1rem!important; margin-top: 1rem!important; display: flex!important; margin-bottom: 1rem!important;'><div><a href="+data[i]["html_url"]+" style='text-decoration:none;'><h1 style='font-size:1rem!important; color:white'>"+nam+"</h1></a><small style='color:white;'>"+typel+"</small></div></div>");
-        if(i==5)
-            loadList();
-        // repolist.insertAdjacentHTML("beforeend","<div style='background-color:"+bgcolor[i%4]+"; border-radius: 0.25rem!important; padding: 1rem!important; margin-top: 1rem!important; display: flex!important; margin-bottom: 1rem!important;'><div><a href="+data[i]["html_url"]+" style='text-decoration:none;'><h1 style='font-size:1rem!important; color:white'>"+nam+"</h1></a><small style='color:white;'>"+typel+"</small></div></div>");
-    }
-    numOfPage = Math.ceil(list.length/numPerPage);
+body, html{
+    height: 100%;
+    margin: 0;
 }
-function nextPage(){
-    if(currPage!=numOfPage)
-        currPage += 1;
-    loadList();
+#firstLook {
+    /* background-image: url('galaxy.gif'); */
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    margin: auto;
+    justify-content: center;
+    display: flex;
+    font-size: xx-large;
+    color: navy;
+    font-weight: bold;
 }
-function previousPage(){
-    if(currPage!=1)
-        currPage -= 1;
-    loadList();
+
+.btn-welcome{
+    cursor: pointer;
+    margin: 25px 0px 25px 0px;
+    border: none;
+    border-radius: 200px;
+    box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 1px 2px rgb(0 0 0 / 23%);
+    font-weight: 400;
+    font-size: larger;
+    background-color: cornflowerblue;
 }
-function loadList(){
-    var beg = ((currPage-1)*numPerPage);
-    var end = beg + numPerPage;
-    pageList = list.slice(beg, end);
-    document.getElementById("list").innerHTML = "";
-    for(r=0; r<pageList.length; r++){
-        document.getElementById("list").innerHTML += pageList[r] + "<br/>";
-    }
-    prev.disabled = currPage==1 ? true : false;
-    next.disabled = currPage==numOfPage ? true : false;
+
+#preface{
+    background-color: #0e0e2a;
+    height: 50%;
+    display: flex
 }
-function landingPage(){
-    var con = document.getElementById("content");
-    var mydiv = document.getElementById("firstLook");
-    mydiv.childNodes[0].remove()
-    mydiv.childNodes[0].remove()
-    mydiv.style.backgroundImage = "url('spaceTravel.gif')";
-    mydiv.style.backgroundSize = "cover";
-    mydiv.style.backgroundPosition = "center";
-    mydiv.style.backgroundRepeat = "no-repeat";
-    setTimeout(function(){mydiv.remove(); con.insertBefore(maincon, con.children[0]);}, 3000);
-    const repo_url = "https://api.github.com/users/akhil-maker/repos";
-    getapi(repo_url);
-    loadList();
+
+#akhilName{
+    width: 50%;
+    border-radius: 80%;
+}
+
+#introPara{
+    width: 50%;
+    color: #d410ed;
+    flex: 50%;
+    text-align: center;
+    align-self: center;
+    font-size: -webkit-xxx-large;
+    font-style: italic;
+    font-family: GT Zirkon,Helvetica,Arial,sans-serif;
+}
+
+#footer{
+    background-color: darkgray;
+    padding: 2rem 7rem 2rem 7rem;
+    display: flex;
+}
+
+#footer:hover{
+    filter: brightness(0.75);
+    color: #4f0ef5;
+}
+
+.logoimg{
+    margin: 0px 10px 0px 10px;
+    width: 37px;
+    height: 37px;
+    object-fit: cover;
+    object-position: 50% 50%;
+}
+
+.projectCard{
+    position: relative;
+    background-size: cover;
+    background-position: 50%;
+    min-height: 430px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+    box-sizing: border-box;
+    margin-left: 1.5rem;
+    background-color: rebeccapurple;
+}
+
+#projectTrain{
+    margin: 0 5rem;
+    display: flex;
+}
+
+.grid{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+    align-items: stretch;
+    margin: 0 5rem 5rem 5rem;
+}
+
+.projCard{
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
+}
+
+.text{
+    position: absolute;
+    color: white;
+    z-index: 2;
+    transition: all ease-in-out .2s;
+    padding: 8rem 0px 0px 1.3rem;
+}
+
+.text>button{
+    background: gray;
+    border: 0;
+    color: white;
+    padding: 10px;
+    width: 100%;
+}
+
+.project-image{
+    border-radius: 5px;
+    box-shadow: 0 4px 6px 0 rgb(34 60 80 / 16%);
+    transition: all ease-in-out .2s;
+    justify-self: center;
+    width: 100%;
+    height: 100%;
+}
+
+.projCard:hover{
+	filter: brightness(0.75);
+	box-shadow: 0 8px 12px 0 rgba(34, 60, 80, .16);
 }
