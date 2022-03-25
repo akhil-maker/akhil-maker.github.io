3d5ce203d4ba851c77d9a7d1b94343fd52ad5f32
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2022-03-25 16:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function landingPage(){
     var mydiv = document.getElementById("firstLook");
     mydiv.childNodes[0].remove()
     mydiv.childNodes[0].remove()
-    // mydiv.style.backgroundImage = "url('spaceTravel.gif')";
+    mydiv.style.backgroundImage = "url('spaceTravel.gif')";
     mydiv.style.backgroundSize = "cover";
     mydiv.style.backgroundPosition = "center";
     mydiv.style.backgroundRepeat = "no-repeat";
